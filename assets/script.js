19319278a45224716f25ae9988ea9f23eabd7aa7
pat paggi <patpaggi@gmail.com>
2020-10-20 10:24:48 -0700

    first

---
+++ assets/script.js
@@ -0,0 +1,13 @@
+
+
+//link momoent to page
+var currentDay = document.getElementById("currentDay");
+var rightNow = moment().format("MMMM Do, YYYY - hh:mm:ss a");
+currentDay.innerText = rightNow;
+//have time pass grey out
+
+
+//link btn to input and save
+
+
+
