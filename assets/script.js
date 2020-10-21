c2548427430687fcacc5a58890710dbfb7ec4a88
pat paggi <patpaggi@gmail.com>
2020-10-21 12:13:53 -0700

    col

--- assets/script.js
+++ assets/script.js
@@ -2,12 +2,17 @@
 
 //link momoent to page
 var currentDay = document.getElementById("currentDay");
-var rightNow = moment().format("MMMM Do, YYYY - hh:mm:ss a");
+var rightNow = moment().format("dddd, MMMM Do YYYY, h:mm:ss a");
 currentDay.innerText = rightNow;
+var currentTime = moment().format("h:mm:ss a");
+
+
 //have time pass grey out
 
 
+$("#8time").click(function () {
+  $("#8quarterHour").toggle();
+})
+
+
 //link btn to input and save
-
-
-
